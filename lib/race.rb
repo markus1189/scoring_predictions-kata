358eafe2689fe306c0e9098606009e6190f9c17f
Markus Hauck <markus1189@gmail.com>
2012-05-15 19:15:18 +0200

    Scoring works on incorrect positions

--- lib/race.rb
+++ lib/race.rb
@@ -1,22 +1,35 @@
 class Race
 
-  class << self
+  def initialize guesses, actual
+    @guesses = guesses
+    @actual = actual
+  end
 
-    def score(guesses, result)
-      points = 0
-      guesses.zip(result).map { |guess, actual|
-        guess == actual
-      }.zip(scores) do |correct, score|
-        correct && points += score
-      end
+  def score
+    score_correct_positions + score_incorrect_positions
+  end
 
+  def score_correct_positions
+    @guesses.zip(@actual).map { |guess, actual|
+      guess == actual
+    }.zip(scores).inject(0) do |points, (correct, score)|
+      correct ? points + score : points
+    end
+  end
+
+  def score_incorrect_positions
+    @guesses.zip(@actual).inject(0) do |points, (guess, actual)|
+      points += 1 if guess != actual && @actual.include?(guess)
       points
     end
+  end
 
-    def scores
-      [15,10,5,3,1]
-    end
+  def scores
+    [15,10,5,3,1]
+  end
 
+  def self.score(guesses, actual)
+    new(guesses, actual).score
   end
 
 end
--- spec/race_spec.rb
+++ spec/race_spec.rb
@@ -1,11 +1,14 @@
 require 'race'
 describe Race do
+  it  "should score mixed results" do
+    Race.score([1,5,3,2,7], [7,5,1,9,8]).should == 1+10+1
+  end
+
   it "should score an completely correct prediction with 34 points" do
     Race.score([1,2,3,4,5], [1,2,3,4,5]).should == 34
   end
 
   context "guessing one position" do
-
     it "should score 15 points for the first" do
       Race.score([1,2,3,5], [1,42,42,42,42]).should == 15
     end
@@ -25,5 +28,13 @@ describe Race do
     it "should score 1 points for the fifth" do
       Race.score([1,2,3,4,5], [42,42,42,42,5]).should == 1
     end
+  end
+
+  it "should score 3 correct guesses" do
+    Race.score([1,4,6,8,2], [1,99,6,8,11]).should == 15+5+3
+  end
+
+  it "should score wrong positions with 1" do
+    Race.score([1,2,3,4,5], [5,4,1,2,3]).should == 5
   end
 end
